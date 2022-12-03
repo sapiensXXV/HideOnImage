9140691c7b9cf47bb51843ee4d285e6bfc54120b
Jaehoon-So <jhspacelover@naver.com>
2022-12-04 04:47:46 +0900

    [Feat] CIFilter extension

---
+++ Sources/HideOnImage/Extension/CIImage+Blur.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jaehoon So on 2022/12/04.
+//
+
+import UIKit
+
+
+extension CIImage {
+
+    
+    
+    public enum FilterType: String {
+        case CIGuassianFilter
+    }
+}
+
---
+++ Sources/HideOnImage/Mosaic+BlurProcess.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jaehoon So on 2022/12/04.
+//
+
+import UIKit
+
+
+extension Mosaic {
+//    public func applyFsdfilter() {
+//        guard let ciImage = CIImage(image: image),
+//              let blurredImage = ciImage.gaussianBlur(),
+//              let maskImage = ciImage.maskAreas(selectedFaceRects),
+//              let blendedImage = ciImage.blendWithMask(maskImage, inputImage: blurredImage),
+//              let cgImage = BlurredImageViewModel.context.createCGImage(
+//                blendedImage,
+//                from: blendedImage.extent
+//              )
+//        else {
+//            blurCompletionHandler?(nil)
+//            return
+//        }
+//
+//        blurCompletionHandler?(UIImage(cgImage: cgImage))
+//    }
+    
+    public func applyMosaic() {
+        guard let image = self.currentImage,
+              !detectBoundInfo.isEmpty else {
+            // TODO: - 에러처리, 감지된 얼굴 없음 알림 등
+            return
+        }
+        
+        guard let ciImage = CIImage(image: image),
+              let mosaicImage = ciImage
+        
+    }
+    
+}
